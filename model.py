0aa228e42919d9d2d64271cb7700b418b24f14ea
Chethaka <071c.krishanthi@gmail.com>
2024-12-29 17:30:16 +0530

    data preprocess started

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from ucimlrepo import fetch_ucirepo
+from sklearn.model_selection import train_test_split
+
 
 # Adjust display options
 pd.set_option('display.max_columns', None)  # Show all columns
@@ -9,25 +10,42 @@ pd.set_option('display.max_rows', None)     # Show all rows (use with caution fo
 pd.set_option('display.width', None)        # Adjust console width to fit all columns
 pd.set_option('display.max_colwidth', None) # Show full content of each column
 
-# Check the file path
-file_path = r"D:\IIT\2nd Year\CM2604 ML\Coursework\bank.csv"
-print(f"File path: {file_path}")  # Debug the file path
 
-# Ensure the file path is correct and the file exists
-try:
-    data = pd.read_csv(file_path, delimiter=';', quotechar='"')
-    print("Data loaded successfully!")
-    print(data.head())  # Display the first few rows
-except FileNotFoundError:
-    print("Error: File not found. Check the file path.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+file_path = r"D:\IIT\2nd Year\CM2604 ML\Coursework\bank-full.csv"
 
+# Create the CSV file data into correct columns
 data = pd.read_csv(file_path, delimiter=';', quotechar='"')
+print("Data loaded successfully!")
+print(data.head(20))
 
-# Step 2: Save the cleaned data (optional)
-output_path = "cleaned_dataset.csv"
-data.to_csv(output_path, index=False)
+# get the columns and rows available
+print(data.shape)
 
-print("Data cleaned successfully!")
-print(data.head())  # Display the first few rows of the cleaned data
+# check weather data is balance or not
+no_count = (data['y'] == 'no').sum()
+print(f"Number of values which has no = {no_count}")
+
+yes_count = (data['y'] == 'yes').sum()
+print(f"Number of values which has yes = {yes_count}")
+
+# According to above information we can see that dataset is Unbalance
+
+# check if null values are available or not
+print(data.info())
+
+# plot the data unbalance
+y = [yes_count,no_count]
+x = ['yes','no']
+inBlncFig,inBlncPlt = plt.subplots()
+inBlncPlt.bar(x, y, color=['green', 'red'])
+inBlncPlt.set_title('Unbalance dataset')
+inBlncFig.show()
+plt.pause(5)
+
+# Data Preprocess
+
+# check the unique values in each column
+for col in data.columns:
+    print(col)
+    print(data[col].unique())
+
